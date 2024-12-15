ec4a6e2fc87c5117356d8252946d4543dda59281
rodmorenocr <rodmorenocr@gmail.com>
2024-12-15 12:36:22 +0100

    prueba

--- Programa/src/Bienvenido.java
+++ Programa/src/Bienvenido.java
@@ -16,7 +16,7 @@ public class Bienvenido extends javax.swing.JFrame {
         this.setTitle("Aura Boutique - Inicio");
     }
 
-    // hola soy rodrigo
+
 
     /**
      * This method is called from within the constructor to initialize the form.
